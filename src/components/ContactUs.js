fed37a3696a10b0c69ecdac957c4549a7ab1515f
Ved Prakash <vedprakash182001@gmail.com>
2024-05-31 14:16:09 +0530

    db

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const Host = "http://localhost:8000"
+
 
 
 
 export default function ContactUs() {
+  const Host = process.env.REACT_APP_HOST;
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     const response = await fetch(`${Host}/message`, {
