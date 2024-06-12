7cb30a594aafe7bbe1911dc9c9b39f57597e1e54
Ved Prakash <vedprakash182001@gmail.com>
2024-06-13 03:31:42 +0530

    Completed

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -7,7 +7,7 @@ export default function FirstSection() {
         <div className='row' style={{marginTop:'20px'}}>
           <div className='col-md-2'></div>
           <div className='col-md-4 order-2 order-md-1 align-self-center' >
-            <p style={{fontSize:'40px', fontWeight:'500',marginTop:'35px'}}>SEATECH</p>
+            <p style={{fontSize:'40px', fontWeight:'500',marginTop:'35px'}}>SEATECHp</p>
             <p style={{fontSize:'30px', fontWeight:'500'}}>Bridging the Human & Technology Divide</p>
           </div>
           <div className='col-md-4 d-flex justify-content-center order-1 order-md-2' style={{height:'400px'}}>
