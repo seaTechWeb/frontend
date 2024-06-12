2818b7e550ccd00f1b34b369ee1cc99304b31dc6
Ved Prakash <vedprakash182001@gmail.com>
2024-06-13 03:35:22 +0530

    Completed

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -11,7 +11,7 @@ export default function FirstSection() {
             <p style={{fontSize:'30px', fontWeight:'500'}}>Bridging the Human & Technology Divide</p>
           </div>
           <div className='col-md-4 d-flex justify-content-center order-1 order-md-2' >
-            <img  className='fit-image' style={{ width:'90%' }} src='/Images/FirstSection/Screenshot from 2024-06-13 02-18-44.png' alt='Four people' />
+            <img  className='fit-image' style={{ width:'100%' }} src='/Images/FirstSection/Screenshot from 2024-06-13 02-18-44.png' alt='Four people' />
           </div>
         </div>
       </div>
