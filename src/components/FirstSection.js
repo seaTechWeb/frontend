fec2bac700254ce43f31baaf4a096690fe068c83
Ved Prakash <vedprakash182001@gmail.com>
2024-06-13 03:33:46 +0530

    Completed

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -7,11 +7,11 @@ export default function FirstSection() {
         <div className='row' style={{marginTop:'20px'}}>
           <div className='col-md-2'></div>
           <div className='col-md-4 order-2 order-md-1 align-self-center' >
-            <p style={{fontSize:'40px', fontWeight:'500',marginTop:'35px'}}>SEATECHp</p>
+            <p style={{fontSize:'40px', fontWeight:'500',marginTop:'35px'}}>SEATECH</p>
             <p style={{fontSize:'30px', fontWeight:'500'}}>Bridging the Human & Technology Divide</p>
           </div>
-          <div className='col-md-4 d-flex justify-content-center order-1 order-md-2' style={{height:'400px'}}>
-            <img  className='fit-image' style={{ height:'90%', width:'auto' }} src='/Images/FirstSection/Screenshot from 2024-06-13 02-18-44.png' alt='Four people' />
+          <div className='col-md-4 d-flex justify-content-center order-1 order-md-2' >
+            <img  className='fit-image' style={{ width:'90%' }} src='/Images/FirstSection/Screenshot from 2024-06-13 02-18-44.png' alt='Four people' />
           </div>
         </div>
       </div>
