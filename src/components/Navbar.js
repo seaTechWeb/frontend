759af1db7e3957a8d3d024f79bfd49a03bf61cbc
Kamthegabru <154924574+Kamthegabru@users.noreply.github.com>
2025-02-27 15:39:46 -0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,7 @@ export default function Navbar() {
                 </ul>
               </li>
               <li className="nav-item mx-3">
-                <HashLink className="nav-link" to="/#ourCompany" onClick={handleNavLinkClick}>Our Company</HashLink>
+                <HashLink className="nav-link" to="/#ourCompanies" onClick={handleNavLinkClick}>Our Companies</HashLink>
               </li>
               <li className="nav-item mx-3">
                 <HashLink className="nav-link" to="/#clients-spotlight" onClick={handleNavLinkClick}>Clients Spotlight</HashLink>
